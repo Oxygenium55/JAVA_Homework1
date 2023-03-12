f22cc3f8240d52fabca74c62aea472ab42aa5e07
Oxygen <grandnorik@mail.ru>
2023-03-12 19:02:39 +0600

    task3 done

--- task3.java
+++ task3.java
@@ -5,6 +5,46 @@
 
 package homework1;
 
+import java.util.Scanner;
+
 public class task3 {
-    
-}
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите первое число: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Введите оператор: (+, -, *, /) ");
+        char operation = scanner.next().charAt(0);
+
+        System.out.println("Введите второе число: ");
+        double num2 = scanner.nextDouble();
+        
+        if (!(operation == '+' || operation == '-' || operation == '*' || operation == '/')) {
+            System.err.println("Неверное значение.");
+        }
+        if (operation == '/' && num2 == 0.0) {
+            System.err.println("Второе число не может быть равно 0.");
+        }
+        switch (operation) {
+            case '+':
+                System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+                break;
+            case '-':
+                System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+                break;
+            case '*':
+                System.out.println(num1 + " x " + num2 + " = " + (num1 * num2));
+                break;
+            case '/':
+                System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
+                break;
+            default:
+                System.err.println("Неверное значение.");
+                break;
+        }
+
+
+    }
+   }
