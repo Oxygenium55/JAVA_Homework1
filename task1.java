caa8ef3e955c282c72e0771a69980c4373f88468
Oxygen <grandnorik@mail.ru>
2023-03-12 17:17:50 +0600

    task1 done

--- task1.java
+++ task1.java
@@ -5,6 +5,23 @@
 
 package homework1;
 
+import java.util.Scanner;
+
 public class task1 {
-    
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        System.out.print("Enter any number, please: ");
+        int n = num.nextInt();
+        num.close();
+        int sum = 0;
+        int factorial = 1;
+        for (int i = 1; i < n + 1; i++) {
+            sum += i;
+        }
+        for (int i = 1; i < n + 1; i++) {
+            factorial *= i;
+        }
+
+        System.out.println("Треугольное число " + n + " равно " + sum + ". Факториал " + n + " равен " + factorial);
+    }
 }
